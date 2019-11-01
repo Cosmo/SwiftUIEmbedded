2ab03efae491f635bd5065036e72ffc7eb6b43d0
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-11-01 02:37:36 +0100

    Build tree for ConditionalContent

---
+++ Sources/SwiftUIEmbedded/ViewExtensions/ConditionalContent.swift
@@ -0,0 +1,20 @@
+import OpenSwiftUI
+
+extension _ConditionalContent: ViewBuildable {
+    public func buildDebugTree(tree: inout Node, parent: Node) {
+        switch _storage {
+        case .first(let content):
+            if let element = content as? ViewBuildable {
+                element.buildDebugTree(tree: &tree, parent: parent)
+            } else {
+                print("Non-ViewBuildable detected. \(content)", #function)
+            }
+        case .second(let content):
+            if let element = content as? ViewBuildable {
+                element.buildDebugTree(tree: &tree, parent: parent)
+            } else {
+                print("Non-ViewBuildable detected. \(content)", #function)
+            }
+        }
+    }
+}
