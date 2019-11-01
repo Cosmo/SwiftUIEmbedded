eb3e78cc0d5647e69c29f224abbbd35b904df59d
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-11-01 02:38:23 +0100

    Remove code duplication

--- Sources/SwiftUIEmbedded/ViewExtensions/HStack.swift
+++ Sources/SwiftUIEmbedded/ViewExtensions/HStack.swift
@@ -5,20 +5,8 @@ extension HStack: ViewBuildable {
         let node = Node(value: HStackDrawable())
         parent.addChild(node: node)
         
-        let contentMirror = Mirror(reflecting: _content)
-        if String(describing: contentMirror.subjectType).starts(with: "TupleView") {
-            let (_, tupleValue) = contentMirror.children.first!
-            let tupleMirror = Mirror(reflecting: tupleValue)
-            
-            for (_, element) in tupleMirror.children {
-                if let element = element as? ViewBuildable {
-                    element.buildDebugTree(tree: &tree, parent: node)
-                }
-            }
-        } else if let element = _content as? ViewBuildable {
-            element.buildDebugTree(tree: &tree, parent: node)
-        } else {
-            print("WTF?: \(String(describing: contentMirror.subjectType))")
+        ViewExtractor.extractViews(contents: _content).forEach {
+            $0.buildDebugTree(tree: &tree, parent: node)
         }
     }
 }
---
+++ Sources/SwiftUIEmbedded/ViewExtractor.swift
@@ -0,0 +1,35 @@
+import OpenSwiftUI
+
+internal struct ViewExtractor<Content>: View where Content: View {
+    internal static func extractViews(contents: Content) -> [ViewBuildable] {
+        var buildables = [ViewBuildable]()
+        
+        let contentMirror = Mirror(reflecting: contents)
+        let isTupleView = String(describing: contentMirror.subjectType).starts(with: "TupleView")
+        
+        if isTupleView, let tupleValue = contentMirror.children.first(where: { $0.label == "value" })?.value {
+            // Multiple Views
+            Mirror(reflecting: tupleValue).children.compactMap {
+                $0.value as? ViewBuildable
+            }.forEach {
+                buildables.append($0)
+            }
+        } else if let element = contents as? ViewBuildable {
+            // Single View
+            buildables.append(element)
+        } else if let element = contents.body as? ViewBuildable {
+            // Custom View
+            buildables.append(element)
+        } else {
+            print("No Idea what's inside.")
+        }
+        
+        return buildables
+    }
+}
+
+extension ViewExtractor {
+    internal var body: Never {
+        fatalError()
+    }
+}
