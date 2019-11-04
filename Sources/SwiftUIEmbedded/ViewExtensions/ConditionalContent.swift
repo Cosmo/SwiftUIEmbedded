c823456e8f532842a6a58b755f8c8c39bbc4278e
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-11-04 18:51:39 +0100

    Update inner workings of _ConditionalContent

--- Sources/SwiftUIEmbedded/ViewExtensions/ConditionalContent.swift
+++ Sources/SwiftUIEmbedded/ViewExtensions/ConditionalContent.swift
@@ -3,13 +3,13 @@ import OpenSwiftUI
 extension _ConditionalContent: ViewBuildable {
     public func buildDebugTree(tree: inout ViewNode, parent: ViewNode) {
         switch _storage {
-        case .first(let content):
+        case .trueContent(let content):
             if let element = content as? ViewBuildable {
                 element.buildDebugTree(tree: &tree, parent: parent)
             } else {
                 print("Non-ViewBuildable detected. \(content)", #function)
             }
-        case .second(let content):
+        case .falseContent(let content):
             if let element = content as? ViewBuildable {
                 element.buildDebugTree(tree: &tree, parent: parent)
             } else {
