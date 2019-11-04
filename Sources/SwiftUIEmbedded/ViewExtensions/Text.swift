bb462f54bf97bd3e649f31f4801e5ac3ecf6a234
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-11-04 18:51:48 +0100

    Update inner workings of Text

--- Sources/SwiftUIEmbedded/ViewExtensions/Text.swift
+++ Sources/SwiftUIEmbedded/ViewExtensions/Text.swift
@@ -2,6 +2,11 @@ import OpenSwiftUI
 
 extension Text: ViewBuildable {
     public func buildDebugTree(tree: inout ViewNode, parent: ViewNode) {
-        parent.addChild(node: ViewNode(value: TextDrawable(text: _content, font: _font, color: _color)))
+        switch _storage {
+        case .verbatim(let content):
+            parent.addChild(node: ViewNode(value: TextDrawable(text: content, font: _font, color: _color)))
+        case .anyTextStorage(let content):
+            parent.addChild(node: ViewNode(value: TextDrawable(text: content.storage, font: _font, color: _color)))
+        }
     }
 }
