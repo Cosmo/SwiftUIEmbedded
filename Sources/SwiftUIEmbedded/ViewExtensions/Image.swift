929ff4273d4a38cf81c7517154640c0d6e53b3a2
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-11-30 15:46:49 +0100

    Add Image initializer for bitmap images

--- Sources/SwiftUIEmbedded/ViewExtensions/Image.swift
+++ Sources/SwiftUIEmbedded/ViewExtensions/Image.swift
@@ -2,6 +2,36 @@ import OpenSwiftUI
 
 extension Image: ViewBuildable {
     public func buildDebugTree(tree: inout ViewNode, parent: ViewNode) {
-        parent.addChild(node: ViewNode(value: ImageDrawable(imageData: _imageData)))
+        if let provider = _provider as? ImageDataProvider {
+            let imageData = provider.imageData
+            parent.addChild(node: ViewNode(value: ImageDrawable(imageData: imageData)))
+        } else {
+            print("Unknown Image")
+        }
+    }
+}
+
+public class ImageDataProvider: AnyImageProviderBox {
+    public var imageData: ImageData
+    
+    init(imageData: ImageData) {
+        self.imageData = imageData
+        super.init()
+    }
+}
+
+extension Image {
+    public init(imageData: ImageData) {
+        self = Image(provider: ImageDataProvider(imageData: imageData))
+    }
+}
+
+public struct ImageData: Equatable {
+    public var bytes: [UInt8]
+    public var size: Size
+    
+    public init(bytes: [UInt8], size: Size) {
+        self.bytes = bytes
+        self.size = size
     }
 }
