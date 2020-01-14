9646ab735c04c7417b81742e197a183569e2a4c0
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2020-01-14 01:06:44 +0100

    Load Pixels from "shim" branch

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Cosmo/OpenSwiftUI.git", .branch("shim")),
         .package(url: "https://github.com/Cosmo/Nodes.git", .branch("master")),
-        .package(url: "https://github.com/Cosmo/Pixels.git", .branch("master")),
+        .package(url: "https://github.com/Cosmo/Pixels.git", .branch("shim")),
         .package(url: "https://github.com/Cosmo/CoreGraphicsShim.git", .branch("master")),
     ],
     targets: [
